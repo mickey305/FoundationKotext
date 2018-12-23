4088f014cfb56bcd5c5843b35c05f33966c7f4b6
K.Misaki <pro.mickey.12091it@gmail.com>
2018-12-23 10:13:15 +0900

    2次元配列作成関数を、分割し初期化の方法を選択できるように修正

--- foundation/src/main/kotlin/com/mickey305/foundation/kotext/v3/Functions.kt
+++ foundation/src/main/kotlin/com/mickey305/foundation/kotext/v3/Functions.kt
@@ -8,8 +8,20 @@ package com.mickey305.foundation.kotext.v3
  * 2次元配列を作成する
  * @return 2次元配列
  */
-inline fun <reified T> createTable(size: Int, noinline init: (Int) -> T): Array<Array<T>> {
-    return Array(size, { Array(size, init) })
+inline fun <reified T> createTable(size: Int, action: (Int, Int) -> T): Array<Array<T>> {
+    return Array(size, { i -> Array(size, { j -> action(i, j) }) })
+}
+
+/**
+ * 2次元配列を作成する
+ * ```
+ * val table = iniTable(10, { 0 }) // width->10, height->10, data->0:Int
+ * val table = iniTable(5, { BigFraction.ZERO }) // width->5, height->5, data->0:BigFraction
+ * ```
+ * @return 2次元配列
+ */
+inline fun <reified T> iniTable(size: Int, action: () -> T): Array<Array<T>> {
+    return createTable(size, { _, _ -> action() })
 }
 
 /**
--- foundation/src/test/kotlin/com/mickey305/foundation/kotext/v3/lang/IMatrixBridgeTest.kt
+++ foundation/src/test/kotlin/com/mickey305/foundation/kotext/v3/lang/IMatrixBridgeTest.kt
@@ -1,7 +1,7 @@
 package com.mickey305.foundation.kotext.v3.lang
 
 import com.mickey305.foundation.kotext.v3.BigFraction
-import com.mickey305.foundation.kotext.v3.createTable
+import com.mickey305.foundation.kotext.v3.iniTable
 import org.apache.commons.lang3.builder.ToStringBuilder
 import org.junit.After
 import org.junit.Before
@@ -22,7 +22,7 @@ class IMatrixBridgeTest : IMatrixBridge {
     @Test
     @Throws(Exception::class)
     fun testCase_01_01() {
-        val tbl = createTable(3, { BigFraction.ZERO })
+        val tbl = iniTable(3, { BigFraction.ZERO })
         tbl[0] = arrayOf(BigFraction(51, 10), BigFraction(25, 10), BigFraction(12, 10))
         tbl[1] = arrayOf(BigFraction(11, 10), BigFraction(15, 10), BigFraction(12, 10))
         tbl[2] = arrayOf(BigFraction(23, 10), BigFraction(25, 10), BigFraction(62, 10))
